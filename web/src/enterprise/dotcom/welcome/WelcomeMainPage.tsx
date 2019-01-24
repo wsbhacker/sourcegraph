12b64ca45f5099b8edb43905addef4b93727adbe
Quinn Slack <qslack@qslack.com>
2019-01-23 22:01:10 -0800

    update copy on main page

--- web/src/enterprise/dotcom/welcome/WelcomeMainPage.tsx
+++ web/src/enterprise/dotcom/welcome/WelcomeMainPage.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
 import { PlatformContextProps } from '../../../../../shared/src/platform/context'
+import { Logo1, Logo2 } from './logos'
 
 interface Props extends ExtensionsControllerProps, PlatformContextProps {
     isLightTheme: boolean
@@ -16,61 +17,148 @@ interface Props extends ExtensionsControllerProps, PlatformContextProps {
 export class WelcomeMainPage extends React.Component<Props> {
     public render(): JSX.Element | null {
         return (
-            <div className="welcome-area">
+            <div className="welcome-main-page">
                 <section className="hero-section">
-                    <div className="container hero-container">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <h2>Sourcegraph</h2>
-                                <h1>Open. For business.</h1>
-                                <p>
-                                    Sourcegraph is a free, open-source, self-hosted code search and intelligence server
-                                    that helps developers find, review, understand, and debug code. Use it with any Git
-                                    code host for teams of any size. Start using it now, then install the Sourcegraph
-                                    Docker image on your private code.
+                    <div className="container hero-container mt-5 pt-3">
+                        <div className="row justify-content-md-center">
+                            <div className="col-md-6">
+                                <img
+                                    className="mb-1 d-none"
+                                    src={
+                                        this.props.isLightTheme
+                                            ? 'https://about.sourcegraph.com/sourcegraph/logo.svg'
+                                            : 'https://about.sourcegraph.com/sourcegraph/logo--light.svg'
+                                    }
+                                />
+                                <img
+                                    className="mb-1"
+                                    style={{ width: '1.5rem', height: '1.5rem' }}
+                                    src="/.assets/img/sourcegraph-mark.svg"
+                                />
+                                {/* <h1>Sourcegraph</h1> */}
+                                <h2 className="mt-2" style={{ fontSize: '24px' }}>
+                                    <span className="font-weight-normal">
+                                        Search,&nbsp;navigate, and review&nbsp;code.
+                                    </span>{' '}
+                                    Find&nbsp;answers.
+                                </h2>
+                                <p>Sourcegraph is a code search and navigation tool for dev teams.</p>
+                                <ul className="pl-3">
+                                    <li>
+                                        <strong>Code search:</strong> fast, works on any commit (no indexing delay),
+                                        with support for regexps, punctuation, diffs, and{' '}
+                                        <a href="https://docs.sourcegraph.com/user/search/queries" target="_blank">
+                                            filters
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <strong>Code intelligence:</strong> go-to-definition and find-references for{' '}
+                                        <a
+                                            href="https://sourcegraph.com/extensions?query=category%3A%22Programming+languages%22"
+                                            target="_blank"
+                                        >
+                                            most languages
+                                        </a>
+                                        , provided by{' '}
+                                        <a href="https://docs.sourcegraph.com/extensions" target="_blank">
+                                            extensions
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <strong>Deep integrations</strong> with your{' '}
+                                        <a
+                                            href="https://docs.sourcegraph.com/integration/browser_extension"
+                                            target="_blank"
+                                        >
+                                            code host and review tools
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/sourcegraph/sourcegraph" target="_blank">
+                                            Open-source
+                                        </a>
+                                        , self-hosted, and free (paid{' '}
+                                        <a href="https://about.sourcegraph.com/pricing" target="_blank">
+                                            Enterprise
+                                        </a>{' '}
+                                        upgrade available)
+                                    </li>
+                                </ul>
+                                <p className="mb-1">
+                                    <a href="https://docs.sourcegraph.com/user/tour" target="_blank">
+                                        See how it's used
+                                    </a>{' '}
+                                    to build better software faster at:
                                 </p>
-                                <p>Upgraded features available for enterprise users.</p>
-                                <a className="btn btn-primary" href="https://docs.sourcegraph.com/#quickstart">
-                                    Deploy Sourcegraph
-                                </a>
-                                <a
-                                    className="btn btn-secondary"
-                                    href="https://github.com/sourcegraph/sourcegraph"
-                                    target="_blank"
-                                >
-                                    Sourcegraph on GitHub
-                                </a>
-                            </div>
-                            <div className="col-lg-6">
-                                <div
-                                    className="bg-secondary border d-flex justify-content-center align-items-center"
-                                    // tslint:disable-next-line:jsx-ban-props
-                                    style={{ width: '360px', height: '240px', textTransform: 'uppercase' }}
-                                >
-                                    demo video placeholder
+                                <div className="welcome-main-page__customer-logos d-flex align-items-center pl-2">
+                                    <Logo1
+                                        className="welcome-main-page__customer-logo welcome-main-page__customer-logo-1 mr-3"
+                                        isLightTheme={this.props.isLightTheme}
+                                    />
+                                    <Logo2
+                                        className="welcome-main-page__customer-logo welcome-main-page__customer-logo-2 mr-4"
+                                        isLightTheme={this.props.isLightTheme}
+                                    />
+                                    <span className="small text-muted">
+                                        &hellip;and thousands of other organizations
+                                    </span>
                                 </div>
                             </div>
+                            <div className="col-md-4 mt-5 pt-4">
+                                <ul className="list-unstyled">
+                                    <li>
+                                        <span className="text-uppercase text-muted font-weight-bold d-block small">
+                                            For your organization's code:
+                                        </span>
+                                        <a
+                                            className="btn btn-primary font-weight-bold mb-1"
+                                            href="https://docs.sourcegraph.com/#quickstart"
+                                        >
+                                            Deploy self-hosted Sourcegraph
+                                        </a>
+                                        <small className="text-muted d-block">
+                                            Docker (recommended, 1-command setup), Kubernetes, other clusters, or
+                                            source. Runs securely on your infra.
+                                        </small>
+                                    </li>
+                                    <li className="mt-4">
+                                        {/* TODO!(sqs): make this different if signed in */}
+                                        <span className="text-uppercase text-muted font-weight-bold d-block small">
+                                            For everyone:
+                                        </span>
+                                        <a
+                                            className="btn btn-secondary mb-1"
+                                            href="https://docs.sourcegraph.com/integration/browser_extension"
+                                        >
+                                            Install browser extension
+                                        </a>{' '}
+                                        <small className="text-muted d-block">
+                                            Adds go-to-definition and find-references to GitHub and other code hosts.
+                                        </small>
+                                    </li>
+                                    <li className="mt-2">
+                                        {/* TODO!(sqs): make this different if signed in */}
+                                        <Link to="/sign-in" target="_blank">
+                                            Sign up on Sourcegraph.com
+                                        </Link>
+                                        <small className="text-muted d-block">For public code only.</small>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-8 mt-5 pt-2 d-flex justify-content-center">
+                                <iframe
+                                    width="728"
+                                    height="410"
+                                    src="https://www.youtube.com/embed/Pfy2CjeJ2N4"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen={true}
+                                />
+                            </div>
                         </div>
                     </div>
                 </section>
-                <section>
-                    <h2>Powering developers at</h2>
-                    <div className="row">
-                        <div className="col-3 welcome-company-logo">
-                            <figure className="welcome-company-logo--image welcome-company-logo__1" />
-                        </div>
-                        <div className="col-3 welcome-company-logo">
-                            <figure className="welcome-company-logo--image welcome-company-logo__2" />
-                        </div>
-                        <div className="col-3 welcome-company-logo">
-                            <figure className="welcome-company-logo--image welcome-company-logo__3" />
-                        </div>
-                        <div className="col-3 welcome-company-logo">
-                            <figure className="welcome-company-logo--image welcome-company-logo__4" />
-                        </div>
-                    </div>
-                </section>
-                <section>
+                <section className="mt-5 d-none">
                     <h2>Advanced code search</h2>
                     <h1>Find. Then replace.</h1>
                     <p>
@@ -85,7 +173,7 @@ export class WelcomeMainPage extends React.Component<Props> {
                         Explore code search
                     </Link>
                 </section>
-                <section>
+                <section className="d-none">
                     <h2>Enhanced code browsing and intelligence</h2>
                     <h1>Mine your language.</h1>
                     <p>
@@ -98,7 +186,7 @@ export class WelcomeMainPage extends React.Component<Props> {
                         Explore code intelligence
                     </Link>
                 </section>
-                <section>
+                <section className="d-none">
                     <h2>Integrations</h2>
                     <h1>Get it. Together.</h1>
                     <p>
@@ -109,7 +197,7 @@ export class WelcomeMainPage extends React.Component<Props> {
                         Explore integrations
                     </Link>
                 </section>
-                <div className="row">
+                <div className="row d-none">
                     <section className="col-lg-6 col-md-12">
                         <h2>Deploy Sourcegraph</h2>
                         <h1>Free. For all.</h1>
@@ -142,7 +230,7 @@ export class WelcomeMainPage extends React.Component<Props> {
                         </a>
                     </section>
                 </div>
-                <section>
+                <section className="d-none">
                     <h2>Open. For business.</h2>
                     <h1>Sourcegraph is open source.</h1>
                     <p>
